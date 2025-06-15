b67bfe55d0ed502a1bb040b4d983febbcfd688c8
REHAMNAKHAN <rehamankhanpathan019@gmail.com>
2025-06-15 19:03:33 +0530

    new

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -4,7 +4,7 @@
 const pagination = document.getElementById("pagination");
 for (let i = 1; i <= 25; i++) {
   const link = document.createElement("a");
-  link.href = `https://www.example.com/page${i}.html`;
+  link.href = `https://hamma2.netlify.app/page${i}.html`;
   link.textContent = i;
   link.className = "page";
   pagination.appendChild(link);
